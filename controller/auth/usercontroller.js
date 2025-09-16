536a3799f224e32841e945f0ec30a75d89579cbc
chankaran2996 <chandrasekaran2996@gmail.com>
2025-09-16 17:21:45 +0530

    middleware is complected for token and chief

--- controller/auth/usercontroller.js
+++ controller/auth/usercontroller.js
@@ -1,4 +1,5 @@
 import User from "../../model/auth/userModel.js";
+import generateToken from "../../utils/generateToken.js";
 
 export const registerUser = async (req, res) => {
   const { email, password, role } = req.body;
@@ -12,4 +13,22 @@ export const registerUser = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: "Invalid email" });
+        }
+        const isMatch = await user.matchPassword(password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid password" });
+        }
+        const token = generateToken(user._id);
+        res.status(200).json({ message: "Login successful", token });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
 };
---
+++ model/auth/institutionDetialsModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const institutionDetailsSchema = new mongoose.Schema({
+  institutionName: {
+    type: String,
+  },
+  partnersName: {
+    type: Array,
+  },
+  contactPersonName: {          
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  phone: {
+    type: Array,
+  },
+  website: {
+    type: String,
+  },
+}, { timestamps: true });
+
+const InstitutionDetails = mongoose.model("InstitutionDetails", institutionDetailsSchema);
+
+export default InstitutionDetails;
